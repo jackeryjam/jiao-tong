e1156d9e139a93968725df9a5ed24994b3cd7e72
Qianguang Zhan <qianguang.zhan@shopee.com>
2021-10-30 17:37:27 +0800

    feat: 🎸 增加event包

---
+++ src/event.ts
@@ -0,0 +1,29 @@
+import { useEffect } from 'react'
+
+export const createEvent = <EventName extends string, EventData = void>() => {
+  type ListenerFn = (val: EventData) => void
+  const listeners: { [key: string]: Array<ListenerFn> } = {}
+  const subscribe = (eventName: EventName, listenerFn: ListenerFn) => {
+    listeners[eventName] = listeners[eventName] || []
+    listeners[eventName].push(listenerFn)
+    return () => {
+      listeners[eventName] = listeners[eventName].filter(fn => fn !== listenerFn)
+    }
+  }
+
+  const dispatch = (eventName: EventName, val: EventData) => {
+    const subscribers = listeners[eventName] || []
+    subscribers.forEach(fn => fn(val))
+  }
+
+  return {
+    useSubscribe: (eventName: EventName, fn: (e: EventData) => void, deps: any[]) => {
+      useEffect(() => {
+        return subscribe(eventName, fn)
+      }, deps)
+    },
+    dispatchEvent: dispatch
+  }
+}
+
+export default createEvent
